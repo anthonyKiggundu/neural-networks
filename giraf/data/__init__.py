05ac043448867f3831672658c962cf60c2e690e7
Anthony Kiggundu <12003998+anthonyKiggundu@users.noreply.github.com>
2026-02-13 23:08:46 +0100

    Initialize data module with loading utilities

---
+++ giraf/data/__init__.py
@@ -0,0 +1,5 @@
+"""Data loading and streaming utilities."""
+
+from giraf.data.data_loader import stream_kpis_from_dataset, prepare_dataset
+
+__all__ = ["stream_kpis_from_dataset", "prepare_dataset"]
